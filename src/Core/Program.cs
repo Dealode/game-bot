b1fa053d515361a178cf7f50b5a47cc56a286746
Vito <karleonev@gmail.com>
2025-10-17 21:33:34 +0300

    inititalize

---
+++ src/Core/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Design;
+
+namespace Core;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        ServiceContainer serviceContainer = new ServiceContainer();
+        
+        AppStateMachine appStateMachine = new AppStateMachine(serviceContainer);
+        appStateMachine.Enter<BootStrapState>();
+    }
+}
+
+internal class AppStateMachine(IServiceContainer serviceContainer)
+{
+    public T Enter<T>()
+    {
+        
+    }
+}
