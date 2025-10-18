295566f457b6409b6e208e7aba75ce75c4096650
Vito <karleonev@gmail.com>
2025-10-18 21:56:11 +0300

    add test browser start

--- src/Core/Program.cs
+++ src/Core/Program.cs
@@ -1,24 +1,32 @@
 ﻿using System.ComponentModel.Design;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Core;
 
 internal static class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         
         ServiceContainer serviceContainer = new ServiceContainer();
         
         AppStateMachine appStateMachine = new AppStateMachine(serviceContainer);
-        appStateMachine.Enter<BootStrapState>();
+        
+        IWebDriver driver = new ChromeDriver();
+        await driver.Navigate().GoToUrlAsync("https://ukr.net");
+        
+        Console.WriteLine("Loaded");
+        
+        //appStateMachine.Enter<BootStrapState>();
     }
 }
 
 internal class AppStateMachine(IServiceContainer serviceContainer)
 {
-    public T Enter<T>()
+    public T Enter<T>() where T : new()
     {
-        
+        return new T();
     }
 }
